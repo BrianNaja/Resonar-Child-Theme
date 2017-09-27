7e4412b401609b98f1635db844bc607d4b53c971
Brian Najafabadi <naja.bp@gmail.com>
2017-09-26 23:02:05 -0400

    copyright update

--- footer.php
+++ footer.php
@@ -15,18 +15,9 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 <!-- 			Footer Copyright -->
 			<div class="site-info">
-				
-<!-- 			Start date with error protection -->
-				<?php function auto_copyright($year = 'auto'){ ?>
-				<?php if(intval($year) == 'auto'){ $year = date('Y'); } ?>
-				<?php if(intval($year) == date('Y')){ echo intval($year); } ?>
-				<?php if(intval($year) < date('Y')){ echo intval($year) . ' - ' . date('Y'); } ?>
-				<?php if(intval($year) > date('Y')){ echo date('Y'); } ?>
-				<?php } ?>
-				
 				<div>AddSpice
 				<span class="sep"> | </span>
-				<?php auto_copyright(); ?>
+				<?php echo date("Y"); ?>
 				</div>			
 			</div><!-- Footer Copyright End  --><!-- .site-info -->
 	</footer><!-- .site-footer -->
